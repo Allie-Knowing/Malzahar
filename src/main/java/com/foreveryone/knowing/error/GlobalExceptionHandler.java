c14e9c2f660ee8093ca1f2f2f920a10e7a2ac82f
kwakdony <kwakdh25@gmail.com>
2021-12-29 15:55:58 +0900

    #3 Add GlobalExceptionHandler

---
+++ src/main/java/com/foreveryone/knowing/error/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.foreveryone.knowing.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(KnowingException.class)
+    public ResponseEntity<ErrorResponse> handleKnowingException(KnowingException e) {
+        String errorDescription = e.getMessage();
+        ErrorCode errorCode = e.getErrorCode();
+        ErrorResponse res = ErrorResponse.of(errorCode, errorDescription);
+
+        return new ResponseEntity<>(res, HttpStatus.valueOf(errorCode.getStatus()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+        String errorDescription = "서버 오류";
+        ErrorResponse res = ErrorResponse.of(errorCode, errorDescription);
+
+        return new ResponseEntity<>(res, HttpStatus.valueOf(errorCode.getStatus()));
+    }
+}
