f83277ac291a05d9d708da0e6af36436cfc117fc
kwakdony <kwakdh25@gmail.com>
2021-12-30 09:57:18 +0900

    #5 Remove JwtTokenProvider 필요없는 메소드 삭제

--- src/main/java/com/foreveryone/knowing/security/JwtTokenProvider.java
+++ src/main/java/com/foreveryone/knowing/security/JwtTokenProvider.java
@@ -1,19 +1,10 @@
 package com.foreveryone.knowing.security;
 
-import com.foreveryone.knowing.error.exceptions.InvalidUserTokenException;
-import com.foreveryone.knowing.security.auth.AuthDetailService;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
 @Component
@@ -49,37 +40,5 @@ public class JwtTokenProvider {
                 .setExpiration(new Date(System.currentTimeMillis() + exp * 1000))
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
-    }
-
-    public Authentication getAuthentication(String token) {
-        UserDetails userDetails = authDetailService.loadUserByUsername(this.getId(token));
-        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
-    }
-
-    public String getId(String token) {
-        return getClaims(token)
-                .getBody()
-                .getSubject();
-    }
-
-    public String resolveToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if (bearerToken != null && bearerToken.startsWith(PREFIX)) {
-            return bearerToken.substring(7);
-        }
-        return null;
-    }
-
-    public boolean validateToken(String jwtToken) {
-        try {
-            return getClaims(jwtToken).getBody().getExpiration().after(new Date());
-        } catch (Exception e) {
-            throw new InvalidUserTokenException("잘못된 토큰입니다.");
-        }
-    }
-
-    public Jws<Claims> getClaims(String token) {
-        return Jwts.parser().setSigningKey(secretKey)
-                .parseClaimsJws(token);
     }
 }
