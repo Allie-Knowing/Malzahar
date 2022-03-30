ab5a8e4d134e82674cadda5b0e3bcdf50f609a8d
kwakdony <kwakdh25@gmail.com>
2022-03-30 23:11:43 +0900

    #21 InquiryRepository 메소드명 변경

--- src/main/java/com/foreveryone/knowing/repository/InquiryRepository.java
+++ src/main/java/com/foreveryone/knowing/repository/InquiryRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface InquiryRepository extends JpaRepository<Inquiry, Integer> {
-    List<Inquiry> findAllByOrderByCategory();
+    List<Inquiry> findAllByOrderByInquiryCategoryCategory();
 }
