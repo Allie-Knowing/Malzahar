19a1297242e1d210ee111449e711141470732d05
kwakdony <kwakdh25@gmail.com>
2022-04-03 11:42:22 +0900

    #23 @GeneratedValue 제거

--- src/main/java/com/foreveryone/knowing/entity/RefreshToken.java
+++ src/main/java/com/foreveryone/knowing/entity/RefreshToken.java
@@ -8,8 +8,6 @@ import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 
@@ -21,8 +19,7 @@ import java.io.Serializable;
 public class RefreshToken implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
 
     @Indexed
     private String refreshToken;
