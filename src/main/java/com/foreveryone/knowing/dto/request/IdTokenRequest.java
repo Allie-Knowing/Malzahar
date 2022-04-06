f0765d04e043be0901024d26605b37521862e5fd
kwakdony <kwakdh25@gmail.com>
2022-04-06 16:34:01 +0900

    test application.yml

--- src/main/java/com/foreveryone/knowing/dto/request/IdTokenRequest.java
+++ src/main/java/com/foreveryone/knowing/dto/request/IdTokenRequest.java
@@ -9,6 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class IdTokenRequest {
 
-    String idToken;
-    String name;
+    private String idToken;
+    private String name;
 }
