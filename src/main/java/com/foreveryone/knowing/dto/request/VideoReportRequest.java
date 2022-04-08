c3e3cb2d42c484bbde8bafe555aab68c68986e4f
kwakdony <kwakdh25@gmail.com>
2022-04-08 11:08:40 +0900

    #18 Rename ReportRequest

--- src/main/java/com/foreveryone/knowing/dto/request/VideoReportRequest.java
+++ src/main/java/com/foreveryone/knowing/dto/request/VideoReportRequest.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.NotNull;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ReportRequest {
+public class VideoReportRequest {
     @NotNull
     private Integer videoId;
     @NotBlank
