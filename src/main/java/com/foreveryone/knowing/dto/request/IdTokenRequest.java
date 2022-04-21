cfb92e2afa32c0d9a0568a89d6da5f1391068b27
kwakdony <kwakdh25@gmail.com>
2022-04-21 20:11:24 +0900

    IdTokenRequest IdToken Null check

--- src/main/java/com/foreveryone/knowing/dto/request/IdTokenRequest.java
+++ src/main/java/com/foreveryone/knowing/dto/request/IdTokenRequest.java
@@ -4,11 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class IdTokenRequest {
 
+    @NotNull
     private String idToken;
     private String name;
 }
