212140da2e68001b0da0f2d1ceede77fdbc80f4e
kwakdony <kwakdh25@gmail.com>
2022-04-27 22:50:27 +0900

    #33 TokenResponse @Builder

--- src/main/java/com/foreveryone/knowing/dto/response/auth/TokenResponse.java
+++ src/main/java/com/foreveryone/knowing/dto/response/auth/TokenResponse.java
@@ -1,13 +1,19 @@
 package com.foreveryone.knowing.dto.response.auth;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class TokenResponse {
 
-    private final String accessToken;
+    private String accessToken;
 
-    private final String refreshToken;
+    private String refreshToken;
+
+    private Boolean isFirstLogin;
 }
