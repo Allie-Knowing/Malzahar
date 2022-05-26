a530da9caa05a49859560bece1cd83a58f7b95b1
kwakdony <kwakdh25@gmail.com>
2022-05-27 00:16:15 +0900

    null 체크

--- src/main/java/com/foreveryone/knowing/dto/request/mypage/FollowingRequest.java
+++ src/main/java/com/foreveryone/knowing/dto/request/mypage/FollowingRequest.java
@@ -3,8 +3,11 @@ package com.foreveryone.knowing.dto.request.mypage;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @NoArgsConstructor
 public class FollowingRequest {
+    @NotNull
     private Integer userId;
 }
